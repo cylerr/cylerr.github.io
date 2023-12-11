2f6f044360bf04e1acdc8c15e88ed8d7bdd07a1f
cylerr <152777406+cylerr@users.noreply.github.com>
2023-12-12 00:34:39 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/flowers.png" type="image/x-icon">
+    <title>For youu</title>
+</head>
+<body>
+    <div class="greetings">
+        <span>H</span>
+        <span>a</span>
+        <span>l</span>
+        <span>o</span>
+        <span>o</span>
+    </div>
+    <div class="description">
+        <span>i have something special for youu</span>
+    </div>
+    <div class="button">
+        <button>
+            <a href="flower.php">Buka disini yaw :D</a>
+        </button>
+    </div>
+</body>
+</html>
